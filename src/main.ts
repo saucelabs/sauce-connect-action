77bbae4d12c9a23eb7af839ada3ba6074557931b
Tom Jenkinson <tom@tjenkinson.me>
2020-12-04 19:15:45 +0000

    add `retryTimeout` option

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
-import {saveState, setFailed} from '@actions/core'
+import {getInput, saveState, setFailed, warning} from '@actions/core'
 import {start} from './start'
 
+const retryDelays = [1, 1, 1, 2, 3, 4, 5, 10, 20, 40, 60].map(a => a * 1000)
+
 async function run(): Promise<void> {
-    const containerId = await start()
-    saveState('containerId', containerId)
+    const retryTimeout = parseInt(getInput('retryTimeout') || '0', 10) * 1000
+    const startTime = Date.now()
+
+    for (let i = 0; ; i++) {
+        try {
+            const containerId = await start()
+            saveState('containerId', containerId)
+            return
+        } catch (e) {
+            if (Date.now() - startTime >= retryTimeout) {
+                break
+            }
+            const delay = retryDelays[Math.min(retryDelays.length - 1, i)]
+            warning(
+                `Error occurred on attempt ${i}. Retrying in ${delay} ms...`
+            )
+            await new Promise(resolve => setTimeout(() => resolve(), delay))
+        }
+        throw new Error('Timed out')
+    }
 }
 
 // eslint-disable-next-line github/no-then
