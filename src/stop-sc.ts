40149720e03f35af47be676b17a50a3571b2e712
Tom Jenkinson <tom@tjenkinson.me>
2022-11-19 13:10:54 +0000

    fix: do not error if sc could not be killed
    which may happen if it already terminated
    
    fixes https://github.com/saucelabs/sauce-connect-action/issues/59

--- src/stop-sc.ts
+++ src/stop-sc.ts
@@ -1,8 +1,16 @@
-import {info} from '@actions/core'
+import {info, warning} from '@actions/core'
 import {exec} from '@actions/exec'
 
 export async function stopSc(pid: string): Promise<void> {
     info(`Trying to stop sc with pid ${pid}`)
-    await exec(`kill ${pid}`)
+    try {
+        await exec(`kill ${pid}`)
+    } catch (e) {
+        warning(
+            `Failed to stop sc (${
+                e instanceof Error ? e.message : e
+            })). It might already have stopped`
+        )
+    }
     info('Done')
 }
